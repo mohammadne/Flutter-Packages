812696b8a1f445c252ab8b4576b713a70ea2e2a4
mohammadne <mohammadne@mail.ru>
2020-07-13 20:56:24 +0430

    complete example

--- fl_localization/example/lib/main.dart
+++ fl_localization/example/lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:fl_localization/fl_localization.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 
-import '../../lib/fl_localization.dart';
-
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  BlocSupervisor.delegate = await HydratedBlocDelegate.build();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -40,16 +44,43 @@ class _UIState extends State<UI> {
       appBar: AppBar(
         title: Text('ui.app_bar').tr(),
       ),
-      body: Column(
-        children: [
-          Text(translate('ui.current_lang')),
-          Text(FlLocalization.currentLocale(context)),
-          Text('ui.available_locales').tr(),
-          ListView.builder(
-            itemCount: FlLocalization.supportedLocalesKey(context).length,
-            itemBuilder: (_, index) => Container(),
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              height: 80,
+              child: Row(
+                children: [
+                  Text(translate('ui.current_lang')),
+                  Text(' : '),
+                  Text(FlLocalization.currentLocale(context)),
+                ],
+              ),
+            ),
+            Text(
+              'ui.available_locales',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.center,
+            ).tr(),
+            ListView.builder(
+              shrinkWrap: true,
+              itemCount: FlLocalization.supportedLocalesKey(context).length,
+              itemBuilder: (_, index) => ListTile(
+                title: Text(
+                  '${index + 1} : ' +
+                      FlLocalization.supportedLocalesKey(context)[index],
+                ),
+                onTap: () {
+                  final locale =
+                      FlLocalization.supportedLocalesKey(context)[index];
+                  FlLocalization.setLocale(context, locale);
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
