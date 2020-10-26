49f43b0b5836a3bf564b3b0cd5918784c4029bd6
mohammadne <mohammadne@mail.ru>
2020-10-26 14:50:45 +0330

    feat: add example

--- fl_hive/lib/fl_hive.dart
+++ fl_hive/lib/fl_hive.dart
@@ -1,1 +1,6 @@
 library fl_hive;
+
+export 'src/instances/multi_normal_fl_hive.dart';
+export 'src/instances/multi_timer_fl_hive.dart';
+export 'src/instances/single_normal_fl_hive.dart';
+export 'src/instances/single_timer_fl_hive.dart';
